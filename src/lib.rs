2956591180e2d100705748bb6705e87f3556234c
Marcel Müller <neikos@neikos.email>
2023-06-21 09:32:40 +0200

    Add initial UFE implementation
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,168 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{marker::PhantomData, ops::Range};
+
+pub mod render;
+
+pub trait AsUserFacingError {
+    fn as_user_facing_error(&self, ctx: &UFEContext) -> UserFacingError;
+}
+
+pub struct UFEContext {
+    _private: PhantomData<()>,
+}
+
+pub struct UFEConverter {
+    convert: fn(&(dyn std::error::Error + 'static), ctx: &UFEContext) -> Option<UserFacingError>,
+}
+
+fn convert<T: AsUserFacingError + std::error::Error + 'static>(
+    e: &(dyn std::error::Error + 'static),
+    ctx: &UFEContext,
+) -> Option<UserFacingError> {
+    if let Some(e) = (&*e).downcast_ref::<T>() {
+        Some(e.as_user_facing_error(ctx))
+    } else {
+        None
+    }
+}
+
+impl UFEConverter {
+    pub const fn for_ufe<T: AsUserFacingError + std::error::Error + 'static>() -> Self {
+        UFEConverter {
+            convert: convert::<T>,
+        }
+    }
+
+    pub const fn custom(
+        f: fn(&(dyn std::error::Error + 'static), ctx: &UFEContext) -> Option<UserFacingError>,
+    ) -> Self {
+        UFEConverter { convert: f }
+    }
+}
+
+#[linkme::distributed_slice]
+pub static UFE_SUPPORTED: [UFEConverter] = [..];
+
+/// An error with all information contained to inform users
+///
+/// A `UserFacingError` is _not_ a [`std::error::Error`]. As it contains information meant to be
+/// consumed by an enduser. The primary use-case is that application authors can be more in control
+/// of their error story.
+///
+/// The typical way this error is constructed is with [`AsUserFacingError`]. Developers are the
+/// encouraged to provide as much information as possible that can be useful to users on how to
+/// resolve or otherwise interpret this error.
+///
+/// Nested errors can be handled by for example writing custom error wrappers. One such example is
+/// [`PotentiallyUnclearError`], which is meant as a last resort 'give something' to users, but can
+/// be, as the name indicates, not particularly helpful. A better path would be to: Simply not tell
+/// about the underlying error and instead say directly how to fix it by looking at what the error
+/// represents.
+///
+/// For example, if your application reads a configuration file and you simply expose any IO Errors
+/// to your user they will have to understand system internas and how _you_ the application
+/// developer interact with the system. The better way would be to generate a [`UserFacingError`]
+/// and instead of directly printing the io error, you generate an appropriate response as to:
+/// 1. Summary of what the error is -> "Could not read the configuration file"
+/// 2. How it occured -> "On startup, the configuration is read from the path given on the command
+///    line."
+/// 3. How it can be fixed
+///     - If its an NOT_FOUND -> "The configuration file at path {absolute_path}, could not be found. Please
+///     ensure the file is at the given path."
+///     - If its an ACCESS_DENIED -> "The configuration file was found but could not be read as
+///     access was denied upon reading it."
+///         - If file owner != app user -> "The file is owned by {file_user}, the application was run as
+///           {my_user}. Please ensure that the user with which the application is run has the rights to
+///           access files by {file_user}. Otherwise, change the owner of the file to align it with the
+///           application user."
+///
+/// This approach is more work, but leads to more informative errors for end users.
+#[derive(Debug)]
+pub struct UserFacingError {
+    pub error: ErrorCause,
+    pub related: Vec<UserFacingError>,
+}
+
+#[derive(Debug)]
+pub struct FileLabel {
+    pub range: Range<usize>,
+    pub message: String,
+}
+
+#[derive(Debug)]
+pub struct FileHighlight {
+    pub path: String,
+    pub content: String,
+    pub labels: Vec<FileLabel>,
+}
+
+#[derive(Debug, Default, derive_setters::Setters)]
+#[setters(strip_option)]
+#[non_exhaustive]
+pub struct ErrorCause {
+    pub summary: String,
+    pub extended_reason: Option<String>,
+    pub file_highlights: Vec<FileHighlight>,
+}
+
+#[derive(Debug)]
+pub struct PotentiallyUnclearError<E>(pub E);
+
+impl AsUserFacingError for PotentiallyUnclearError<&(dyn std::error::Error + 'static)> {
+    fn as_user_facing_error(&self, ctx: &UFEContext) -> UserFacingError {
+        for conv in UFE_SUPPORTED {
+            if let Some(ufe) = (conv.convert)(self.0, ctx) {
+                return ufe;
+            }
+        }
+
+        UserFacingError {
+            error: ErrorCause::default().summary(self.0.to_string()),
+            related: self
+                .0
+                .source()
+                .map(|e| PotentiallyUnclearError(e).as_user_facing_error(ctx))
+                .into_iter()
+                .collect(),
+        }
+    }
+}
+
+impl<E: std::error::Error> From<E> for PotentiallyUnclearError<E> {
+    fn from(value: E) -> Self {
+        Self(value)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::error::Error;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    use crate::{AsUserFacingError, UFEContext, UFEConverter};
+
+    #[allow(dead_code)]
+    fn check_converting_compiles() {
+        fn get_ctx() -> &'static UFEContext {
+            todo!()
+        }
+
+        #[derive(Debug, thiserror::Error)]
+        #[error("Foobar")]
+        struct Foobar;
+
+        impl AsUserFacingError for Foobar {
+            fn as_user_facing_error(&self, _ctx: &crate::UFEContext) -> crate::UserFacingError {
+                crate::UserFacingError {
+                    error: crate::ErrorCause::default(),
+                    related: vec![],
+                }
+            }
+        }
+
+        let conv = UFEConverter::for_ufe::<Foobar>();
+
+        let e: &dyn Error = &Foobar;
+
+        let ctx = get_ctx();
+        (conv.convert)(e.source().unwrap(), ctx).unwrap();
     }
 }
---
+++ src/render.rs
@@ -0,0 +1,75 @@
+use std::fmt::Write;
+
+use ariadne::{ColorGenerator, FnCache, Label, Report};
+
+use super::UserFacingError;
+
+struct Context {
+    _max_width: usize,
+    first_run: bool,
+}
+
+impl Context {
+    fn go_to_inner(&self) -> Context {
+        Context {
+            first_run: false,
+            ..*self
+        }
+    }
+}
+
+pub fn render_for_terminal(error: &UserFacingError, max_width: usize) -> String {
+    let context = Context {
+        _max_width: max_width,
+        first_run: true,
+    };
+    render_for_terminal_inner(error, &context)
+}
+
+fn render_for_terminal_inner(error: &UserFacingError, context: &Context) -> String {
+    let mut output = String::new();
+    writeln!(&mut output, "{}", &error.error.summary).unwrap();
+
+    if let Some(extended) = &error.error.extended_reason {
+        writeln!(&mut output, "\n{}", extended).unwrap();
+    }
+
+    for fh in &error.error.file_highlights {
+        let mut colors = ColorGenerator::new();
+
+        let mut report = Report::build(ariadne::ReportKind::Error, fh.path.as_str(), 0);
+
+        for l in &fh.labels {
+            let color = colors.next();
+            report = report.with_label(
+                Label::new((fh.path.as_str(), l.range.clone()))
+                    .with_message(l.message.clone())
+                    .with_color(color),
+            );
+        }
+
+        let report = report.finish();
+        let mut src = FnCache::new(|_a: &&str| Ok(fh.content.clone()));
+        let mut buf = vec![];
+        report.write_for_stdout(&mut src, &mut buf).unwrap();
+        write!(&mut output, "{}", String::from_utf8(buf).unwrap()).unwrap();
+    }
+
+    if !error.related.is_empty() {
+        if context.first_run {
+            writeln!(&mut output, "Detailed informations:").unwrap();
+        }
+
+        for err in &error.related {
+            let inner_context = context.go_to_inner();
+            write!(
+                &mut output,
+                "{}",
+                render_for_terminal_inner(err, &inner_context)
+            )
+            .unwrap();
+        }
+    }
+
+    output
+}
